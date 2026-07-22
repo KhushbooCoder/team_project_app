3a6d1a3fd8834a374ebb7f533443dcc98f4ef0bd
khushboo kumari <khusboo707112@gmail.com>
2026-07-22 12:10:09 +0530

    i changed this file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:team_project_app/login_screen.dart';
+import 'package:team_project_app/splass_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: SplashScreen(),
     );
   }
 }
--- lib/splass_screen.dart
+++ lib/splass_screen.dart
@@ -21,6 +21,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
+import 'login_screen.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -30,22 +32,22 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _startSplash();
-  // }
+  @override
+  void initState() {
+    super.initState();
+    _startSplash();
+  }
 
-  // void _startSplash() {
-  //   Timer(const Duration(seconds: 3), () {
-  //     Navigator.pushReplacement(
-  //       context,
-  //       MaterialPageRoute(
-  //         builder: (context) => const HomeScreen(), // Next Screen
-  //       ),
-  //     );
-  //   });
-  // }
+  void _startSplash() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const LoginScreen(), // Next Screen
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -118,11 +120,6 @@ class _SplashScreenState extends State<SplashScreen> {
                 color: Color(0xff60708F),
               ),
             ),
-
-
-
-
-
 
 
             const SizedBox(height: 30),
